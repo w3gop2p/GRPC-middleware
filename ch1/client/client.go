fbf8013607e5b4c8d41601d456d0575cace610a8
Serge <kushner.serge@gmail.com>
2023-11-27 09:53:53 +0200

    Added ch2

---
+++ ch1/client/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	shipping "github.com/w3gop2p/ch1/proto"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+func main() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:8080", opts...)
+	if err != nil {
+		log.Fatalf("Failed to connect shipping service. Err: %v", err)
+	}
+
+	defer conn.Close()
+
+	shippingClient := shipping.NewShippingServiceClient(conn)
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	log.Println("Creating shipping...")
+	_, errCreate := shippingClient.Create(ctx, &shipping.CreateShippingRequest{UserId: 23})
+	if errCreate != nil {
+		log.Printf("Failed to create shipping. Err: %v", errCreate)
+	} else {
+		log.Println("Shipping is created successfully.")
+	}
+}
---
+++ ch1/server/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	shipping "github.com/w3gop2p/ch1/proto"
+	"google.golang.org/grpc"
+	"log"
+	"math/rand"
+	"net"
+	"time"
+)
+
+type server struct {
+	shipping.UnimplementedShippingServiceServer
+}
+
+func (s *server) Create(ctx context.Context, in *shipping.CreateShippingRequest) (*shipping.CreateShippingResponse, error) {
+	time.Sleep(2 * time.Second) // simulated delay
+	return &shipping.CreateShippingResponse{ShippingId: 1243}, nil
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+
+	go func() {
+		res := randomFunc(ctx, "a")
+		log.Println(res)
+		cancel()
+	}()
+	go func() {
+		res := randomFunc(ctx, "b")
+		log.Println(res)
+		cancel()
+	}()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", 8080))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	shipping.RegisterShippingServiceServer(grpcServer, &server{})
+	grpcServer.Serve(listener)
+}
+
+func randomFunc(ctx context.Context, name string) string {
+	rand.Seed(time.Now().UnixNano())
+	min := 3
+	max := 7
+	sleepTime := rand.Intn(max-min+1) + min
+	time.Sleep(time.Duration(sleepTime) * 1000000)
+	return "hello from " + name
+}
